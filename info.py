5006e714baef6e18731d0f6ab58964b8cf23f6e8
VarnaX <56477171+VarnaX@users.noreply.github.com>
2021-12-24 16:10:41 +0530

    Added env

--- info.py
+++ info.py
@@ -2,67 +2,68 @@ import re
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
 
-def required_var(var_name, is_int=False):
+def get_var(var_name, default=None, is_int=False, is_bool=False, is_required=False):
     try:
         var = environ[var_name]
         if len(var) == 0:
             raise KeyError
         if is_int:
             var = int(var)
+        if is_bool:
+            if var.lower() in ["true", "yes", "1", "enable", "y"]:
+                var = True
+            elif var.lower() in ["false", "no", "0", "disable", "n"]:
+                var = False
+            else:
+                var = default
         return var
-    except KeyError:
-        print('One or more env variables missing! Exiting now')
-        exit(1)
-    except ValueError:
-        print('One or more int type env variables missing! Exiting now')
-        exit(1)
+    except (KeyError, ValueError):
+        if is_required:
+            print('One or more env variables missing! Exiting now')
+            exit(1)
+        else:
+            return default
+
 
 # Bot information
-API_ID = required_var('API_ID',True)
-API_HASH = required_var('API_HASH')
-BOT_TOKEN = required_var('BOT_TOKEN')
-SESSION = environ.get('SESSION', 'Media_search')
+API_ID = get_var('API_ID', is_int=True, is_required=True)
+API_HASH = get_var('API_HASH', is_required=True)
+BOT_TOKEN = get_var('BOT_TOKEN', is_required=True)
+SESSION = get_var('SESSION', 'Media_search')
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
+CACHE_TIME = get_var('CACHE_TIME', 300, is_int=True)
+USE_CAPTION_FILTER = get_var('USE_CAPTION_FILTER', False, is_bool=True)
+PICS = (get_var('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in required_var('ADMINS').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in get_var('ADMINS', is_required=True).split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in get_var('CHANNELS', '0').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in get_var('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
-auth_grp = environ.get('AUTH_GROUP')
+auth_channel =get_var('AUTH_CHANNEL')
+auth_grp = get_var('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = required_var('DATABASE_URI')
-DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = get_var('DATABASE_URI', is_required=True)
+DATABASE_NAME = get_var('DATABASE_NAME', "EvaMaria")
+COLLECTION_NAME = get_var('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = required_var('LOG_CHANNEL',True)
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
-IMDB = is_enabled((environ.get('IMDB', "True")), True)
-SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
-LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
-MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
-INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
+LOG_CHANNEL = get_var('LOG_CHANNEL', is_int=True, is_required=True)
+SUPPORT_CHAT = get_var('SUPPORT_CHAT', 'TeamEvamaria')
+P_TTI_SHOW_OFF = get_var('P_TTI_SHOW_OFF', False, is_bool=True)
+IMDB = get_var('IMDB', True, is_bool=True)
+SINGLE_BUTTON = get_var('SINGLE_BUTTON', False, is_bool=True)
+CUSTOM_FILE_CAPTION = get_var("CUSTOM_FILE_CAPTION", None)
+IMDB_TEMPLATE = get_var("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌‌‌‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
+LONG_IMDB_DESCRIPTION = get_var("LONG_IMDB_DESCRIPTION", False, is_bool=True)
+SPELL_CHECK_REPLY = get_var("SPELL_CHECK_REPLY", True, is_bool=True)
+MAX_LIST_ELM = get_var("MAX_LIST_ELM", None)
+INDEX_REQ_CHANNEL = get_var('INDEX_REQ_CHANNEL', LOG_CHANNEL, is_int=True)
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
